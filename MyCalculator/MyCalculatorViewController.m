a08f89254d0174644877e4767e7ed4ed3b99b728
Austin White <austingym@gmail.com>
2014-03-09 17:27:54 -0700

    Updated app look and feel

--- MyCalculator/MyCalculatorViewController.m
+++ MyCalculator/MyCalculatorViewController.m
@@ -7,23 +7,72 @@
 //
 
 #import "MyCalculatorViewController.h"
+#import "ShuntingYard.h"
 
 @interface MyCalculatorViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *lblDisplay;
 
 @end
 
 @implementation MyCalculatorViewController
 
+@synthesize lblDisplay;
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Change status bar to white color
+    [self setNeedsStatusBarAppearanceUpdate];
+    
+    
+    // Testing...
+    NSDictionary *tokens = @{
+                             @"+": @{@"precedence": @0, @"associativity": @"left"},
+                             @"-": @{@"precedence": @0, @"associativity": @"left"},
+                             @"*": @{@"precedence": @1, @"associativity": @"left"},
+                             @"/": @{@"precedence": @1, @"associativity": @"left"},
+                             @"%": @{@"precedence": @1, @"associativity": @"left"},
+    };
+    
+    ShuntingYard *shuntingYard = [[ShuntingYard alloc] initWithOperators:tokens];
+    NSLog(@"%@", [shuntingYard shuntingYardWithTokens:@[@"1", @"+", @"2", @"*", @"3"]]);
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
 }
+
+#pragma mark Calculations
+- (void)push:(NSString *)item onTopOfStack:(NSMutableArray *)stack
+{
+    
+}
+
+- (NSString *)pop:(NSMutableArray *)stack
+{
+    return @"";
+}
+
+#pragma mark Calculator Buttons
+- (IBAction)calculatorButtonPressed:(UIButton *)sender {
+    
+    NSArray *operators = @[@"%", @"√", @"÷", @"×", @"-", @"+", @"±"],
+            *operands  = @[@"1", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"0", @"."];
+    
+    if ([operators containsObject:[sender currentTitle]]) {
+        NSLog(@"Button is an operator: %@", [sender currentTitle]);
+    } else if ([operands containsObject:[sender currentTitle]]) {
+        NSLog(@"Button is an operand: %@", [sender currentTitle]);
+    } else if ([[sender currentTitle] isEqualToString:@"AC"]) {
+        NSLog(@"Clearing screen...");
+    } else if ([[sender currentTitle] isEqualToString:@"="]) {
+        NSLog(@"Calculating...");
+    }
+}
+
+
 
 @end
